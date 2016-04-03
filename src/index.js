e507633cdd5f6f6b1b86d4eb5c0b0ff98a78aa83
Hiroshi Yoshida <hrysd22@gmail.com>
2016-04-03 21:34:36 +0900

    WIP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Editor, EditorState} from 'draft-js';
+
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+
+  render() {
+    return <Editor editorState={this.state.editorState} onChange={this.onChange} ref="editor"/>;
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <MyEditor />,
   document.getElementById('app')
 );
